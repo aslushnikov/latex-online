10398656ef1e9f7f0676fcbe036215bd7b3fd23c
Andrey Lushnikov <aslushnikov@gmail.com>
2012-05-10 03:49:46 +0400

    Refactored process.js
    Now it has its own namespace ^_^

--- process.js
+++ process.js
@@ -4,65 +4,68 @@ var Memcached = require('./memcached.js')
 
 var memcached;
 
-function fetch(options, callback) {
-    var cmd = 'bash ';
-    if (options.fetchType == "file") {
-        cmd += "fetch.sh -f" + options.file;
-    } else if (options.fetchType == "url") {
-        cmd += "fetch.sh -u" + options.url;
-    } else {
-        throw new Error("Wrong options passed to fetch: " + JSON.stringify(options));
-    }
-    exec(cmd, function (error, stdout, stderr) {
-        if (error !== null) {
-            callback(error);
-            return;
-        }
+function RequestProcessor(options) {
+    var self = this;
+    this.options = options;
 
-        var lines = stdout.split('\n');
-        var tmpdir = lines[0];
-        var filename = lines[1];
-        var md5 = lines[2];
-        callback(null, tmpdir, filename, md5);
-    });
-}
-
-function compile(tmpdir, filename, md5, callback) {
-    var cmd = 'bash compile.sh ' + filename;
-    console.log(cmd);
-    exec(cmd, function (error, stdout, stderr) {
-        if (error !== null) {
-            exec('bash cleanup.sh ' + tmpdir);
-            console.error("cmd ERR: " + error);
-            error = new Error(error.toString() + "\n" + stderr);
-            callback(error);
-            return;
+    this.fetch = function(options, callback) {
+        var cmd = 'bash ';
+        if (options.fetchType == "file") {
+            cmd += "fetch.sh -f" + options.file;
+        } else if (options.fetchType == "url") {
+            cmd += "fetch.sh -u" + options.url;
+        } else {
+            throw new Error("Wrong options passed to fetch: " + JSON.stringify(options));
         }
-        var compiledFileName = stdout.trim();
-        console.log("Compiled file saved as: " + compiledFileName);
-        fs.readFile(compiledFileName, function(err, data){
-            if (err) {
-                console.error("File read error: " + err);
-                exec('bash cleanup.sh ' + tmpdir);
-                callback(err);
+        exec(cmd, function (error, stdout, stderr) {
+            if (error !== null) {
+                callback(error);
                 return;
             }
-            console.log("Successfully read " + data.length + " bytes");
 
-            memcached.store(md5, data);
-
-            console.log("calling callback");
-            callback(null, data);
-            console.log("removing file");
-            exec('bash cleanup.sh ' + tmpdir);
+            var lines = stdout.split('\n');
+            var tmpdir = lines[0];
+            var filename = lines[1];
+            var md5 = lines[2];
+            callback(null, tmpdir, filename, md5);
         });
-    });
-}
+    }
 
-function createFetchCallback(callback) {
+    this.compile = function (tmpdir, filename, md5, callback) {
+        var cmd = 'bash compile.sh ' + filename;
+        console.log(cmd);
+        exec(cmd, function (error, stdout, stderr) {
+            if (error !== null) {
+                exec('bash cleanup.sh ' + tmpdir);
+                console.error("cmd ERR: " + error);
+                error = new Error(error.toString() + "\n" + stderr);
+                callback(error);
+                return;
+            }
+            var compiledFileName = stdout.trim();
+            console.log("Compiled file saved as: " + compiledFileName);
+            fs.readFile(compiledFileName, function(err, data){
+                if (err) {
+                    console.error("File read error: " + err);
+                    exec('bash cleanup.sh ' + tmpdir);
+                    callback(err);
+                    return;
+                }
+                console.log("Successfully read " + data.length + " bytes");
+
+                memcached.store(md5, data);
+
+                console.log("calling callback");
+                callback(null, data);
+                console.log("removing file");
+                exec('bash cleanup.sh ' + tmpdir);
+            });
+        });
+    }
+
     function fetchCallback(err, tmpdir, filename, md5) {
         if (err)  {
-            callback(err);
+            self.callback(err);
             return;
         }
         console.log("Fetched file saved as " + filename + " with md5 = " + md5);
@@ -74,26 +77,31 @@ function createFetchCallback(callback) {
                 } else {
                     console.error("Memcached doesn't have anything for key = " + md5);
                 }
-                compile(tmpdir, filename, md5, callback);
+                self.compile(tmpdir, filename, md5, self.callback);
             } else {
                 console.log("fetched " + result.length + " bytes from memcache for KEY " + md5);
                 // don't forget to clear the fetched file
                 exec('bash cleanup.sh ' + tmpdir);
                 // call callback
-                callback(null, result);
+                self.callback(null, result);
             }
         });
     }
 
-    return fetchCallback;
-}
-
-function processUrl(url, callback) {
-    fetch({fetchType: "url", url: url}, createFetchCallback(callback));
+    this.process = function(callback) {
+        this.callback = callback;
+        this.fetch(this.options, fetchCallback);
+    }
 }
 
 function processFile(file, callback) {
-    fetch({fetchType: "file", file: file}, createFetchCallback(callback));
+    var rp = new RequestProcessor({fetchType: "file", file: file});
+    rp.process(callback);
+}
+
+function processUrl(url, callback) {
+    var rp = new RequestProcessor({fetchType: "url", url: url});
+    rp.process(callback);
 }
 
 module.exports = function(options) {
