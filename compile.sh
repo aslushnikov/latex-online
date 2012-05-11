5222a4cd8a88099c9f7295daa73adf0c1f5ab276
Andrey Lushnikov <aslushnikov@gmail.com>
2012-05-11 13:14:48 +0400

    Refactoring: simplifying compile.sh script

--- compile.sh
+++ compile.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
-
+#
+# Compiles given source, running pdflatex on it couple of times
+#
 # Usage
-#   bash compile.sh ./tmp/lusha.RNDSuffix/file.tex [./tmp/lusha.RNDSuffix]
-# if the second argument (output directory) is not specified,
-# then the result will be stored in the same dir as .tex file is
+#   bash compile.sh [tmpdir] [target]
+#
+# Output
+#   On success returns name of produced PDF, otherwise
+#   exits with code 1
 
 #####################
 # F U N C T I O N S #
 #####################
 
 function compile {
-    outputStream="/dev/null"
     opts="-interaction nonstopmode -output-directory $tmpdir"
     if [[ "$1" == "draft" ]]; then
-        pdflatex $opts -draftmode $filename >/dev/null
+        pdflatex $opts -draftmode $target >/dev/null
     else
-        pdflatex $opts $filename >&2
+        pdflatex $opts $target >&2
     fi
-
 }
 
 ##############
 # START HERE #
 ##############
 
-filename=$1
-tmpdir=$2
-if $tmpdir; then
-    tmpdir=${1%/*.*}
-    #echo "Getting tmp dir from file name. tmpdir=$tmpdir"
+if [[ $# != 2 ]]; then
+    echo "Not enough arguments!"
+    echo "Usage: bash compile.sh [tmpdir] [target]"
+    exit 1
 fi
-noExtension=${filename%.*}
 
-has_toc=`grep -l '\tableofcontents' $filename | wc -l`
-if [ $has_toc == "1" ]; then
+tmpdir=${1%/}
+target=$2
+
+has_toc=`grep -l '\tableofcontents' $target | wc -l`
+if [[ ${has_toc// /} -gt 1 ]]; then
     compile "draft"
     compile
 else
     compile
 fi
-# if no PDF created, then report error with stdout stream
-if [[ ! -e $noExtension.pdf ]]; then
-    echo "No pdf created"
-    exit 1
-fi
 
-echo $noExtension.pdf
+# if no PDF created, then exitcode 1
+pdfCreated=$tmpdir/`basename $target`
+pdfCreated=${pdfCreated%.*}.pdf
+if [[ ! -e $pdfCreated ]]; then
+    exit 1
+else
+    echo $pdfCreated
+fi
 
