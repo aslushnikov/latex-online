d1619aaa96c1421aa36ed6344827763f42f84bfd
Andrey Lushnikov <lushnikov@chromium.org>
2016-05-05 13:34:22 +0200

    Use production mode for docker container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,4 @@ rm -rf /var/www
 git clone https://github.com/aslushnikov/latex-online /var/www
 cd /var/www
 npm install .
-export NODE_ENV=development && export VERSION=$(git rev-parse HEAD) && node app.js
+export NODE_ENV=production && export VERSION=$(git rev-parse HEAD) && node app.js
