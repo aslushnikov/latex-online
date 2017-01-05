ea077667f843decb4bfec5edc72225098dd87958
Philipp von Kirschbaum <KirschbaumP@users.noreply.github.com>
2017-01-05 12:14:39 +0000

    Update docker-entrypoint.sh (#22)
    override existing links

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ rm -rf /var/www
 # Copy and install the latest & greatest Latex-Online
 git clone https://github.com/aslushnikov/latex-online /var/www
 # make latexrun available globally
-ln -s /var/www/latexrun/latexrun /usr/local/bin/latexrun
+ln -sf /var/www/latexrun/latexrun /usr/local/bin/latexrun
 cd /var/www
 npm install .
 export NODE_ENV=production && export VERSION=$(git rev-parse HEAD) && node app.js
