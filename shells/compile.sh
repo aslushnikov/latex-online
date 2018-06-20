a7c09b28e1f5fa5cc5dbf9a5e910d1d2db207593
Jose Luis Diaz (pep) <jldiaz@uniovi.es>
2018-06-20 23:02:33 +0200

    Changing locale to support utf-8 filenames

--- shells/compile.sh
+++ shells/compile.sh
@@ -31,6 +31,7 @@ command=$3
 outputFile=$4
 logFile=$5
 
+export LC_ALL=C.UTF-8
 cd $rootdir
 LATEXRUN="$(dirname $0)"/../latexrun/latexrun
 PYTHONUNBUFFERED=true $LATEXRUN --verbose-cmd --latex-cmd=${command} -Wall -o $outputFile $target &>$logFile
