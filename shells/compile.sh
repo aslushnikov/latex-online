80e2c166d7c637432a7e305da56047c876275992
Andrey Lushnikov <lushnikov@chromium.org>
2021-05-23 02:43:44 -0700

    fix: properly handle spaces in path names
    Fixes #53

--- shells/compile.sh
+++ shells/compile.sh
@@ -25,22 +25,22 @@ if [[ $# != 5 ]]; then
     exit 1
 fi
 
-rootdir=${1%/}
-target=$2
-command=$3
-outputFile=$4
-logFile=$5
+rootdir="${1%/}"
+target="$2"
+command="$3"
+outputFile="$4"
+logFile="$5"
 
 export LC_ALL=C.UTF-8
 cd $rootdir
 LATEXRUN="$(dirname $0)"/../latexrun/latexrun
-PYTHONUNBUFFERED=true $LATEXRUN --verbose-cmd --latex-cmd=${command} -Wall -o $outputFile $target &>$logFile
+PYTHONUNBUFFERED=true $LATEXRUN --verbose-cmd --latex-cmd="${command}" -Wall -o "${outputFile}" "${target}" &>"${logFile}"
 
 # if no PDF created, then exitcode 1
-if [[ ! -e $outputFile ]]; then
+if [[ ! -e "${outputFile}" ]]; then
     echo "ERROR: failed to locate PDF file."
     exit 1
 else
-    echo "SUCCESS: created $outputFile"
+    echo "SUCCESS: created ${outputFile}"
 fi
 
