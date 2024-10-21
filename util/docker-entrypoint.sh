06a69688b99db04d06d0b856c66176d9370ef314
Clemens John <clemens.john@john-it.com>
2024-10-21 19:59:12 +0200

    fix: use "npm ci" instead of "npm install" to prevent from getting package upgrades that break the app. (#101)

--- util/docker-entrypoint.sh
+++ util/docker-entrypoint.sh
@@ -6,7 +6,7 @@ rm -rf /var/www
 # Copy and install the latest & greatest Latex-Online
 git clone https://github.com/aslushnikov/latex-online /var/www
 cd /var/www
-npm install .
+npm ci .
 
 export NODE_ENV=production
 export VERSION=$(git rev-parse HEAD)
